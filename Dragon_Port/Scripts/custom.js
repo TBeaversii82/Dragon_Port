a4bdb27d40844b1b22ce8c3f324744786deb8972
TBeaversii82 <70877109+TBeaversii82@users.noreply.github.com>
2020-10-04 16:33:02 -0400

    Initial commit

---
+++ Dragon_Port/Scripts/custom.js
@@ -0,0 +1,17 @@
+/*
+(function($) { //<< begin no conflict function
+ $(document).ready(function() { //<< begin doc ready
+   
+   //with portotype
+   
+ }); //<< end doc ready
+})(jQuery); //<< end no conflict function
+*/
+
+/*
+ $(document).ready(function() { //<< begin doc ready
+   
+   
+   
+ }); //<< end doc ready
+*/
